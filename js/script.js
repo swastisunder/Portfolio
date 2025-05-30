6eef77abdc5c0d13f617d9f91bb416bcf2dc549e
swastisunder <badatyaswastisunder@gmail.com>
2025-05-30 11:59:35 +0530

    style: implement dark mode and style switcher functionality

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Typing animation
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Frontend Developer",
+    "Full Stack Developer",
+    "React Developer",
+    "Web Developer",
+    "Java Developer",
+    "Database Developer",
+  ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+});
